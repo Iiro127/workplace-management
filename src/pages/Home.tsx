a774b1cb59641928fa362d28082e7715b816950d
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-01-19 13:05:05 +0200

    Added side bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.tsx';
+import Sidenav from '../components/Sidenav/Sidenav.tsx';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
+      <Sidenav />
       <main>
         <h1>Hello world</h1>
       </main>
