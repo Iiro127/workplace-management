130c7443f7833fffc798e37a5c8651a4fe604b94
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-01-23 17:22:20 +0200

    Added pages and navigation to them

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 import Navbar from '../components/Navbar/Navbar.tsx';
 import Sidenav from '../components/Sidenav/Sidenav.tsx';
 
-const Home: React.FC = () => {
+const Schedule: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Sidenav />
       <main>
-        <h1>Home page</h1>
+        <h1>Schedule page</h1>
       </main>
     </div>
   );
 };
 
-export default Home;
+export default Schedule;
