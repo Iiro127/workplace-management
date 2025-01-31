4edcac50052c06ac395b1cc3a6e32eed77a30151
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-01-31 17:02:10 +0200

    Projects view progress

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import styles from './Project.module.css';
 
-interface ProjectProps {
-    title: string;
+interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    isActive: boolean;
 }
 
-const Project: React.FC<ProjectProps> = ({ title }) => {
+interface ProjectProps {
+    id: string;
+    title: string;
+    customer: string;
+    dateAdded: string;
+    finishEstimate: string;
+    phase: string;
+    manager: User;
+    members: User[];
+}
+
+
+const Project: React.FC<ProjectProps> = ({ title, customer, dateAdded, finishEstimate, phase }) => {
     return (
         <div className={styles.projectitem}>
-            <h2>{title}</h2>
+            <div className={styles.details}>
+                <h2 className={styles.title}>{title}</h2>
+                <p className={styles.customer}>{customer}</p>
+            </div>
+            <div className={styles.dateAddedContainer}>
+                <p>{dateAdded}</p>
+            </div>
+            <div className={styles.finishEstimateContainer}>
+                <p>{finishEstimate}</p>
+            </div>
+            <div className={styles.phaseContainer}>
+                <div className={`${styles.phase} ${styles[phase.toLowerCase()]}`}>
+                    <p>{phase}</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,31 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import styles from './ProjectList.module.css';
 import Project from '../Project/Project.tsx';
 
 const projects = [
     {
-        title: "Inventory helper",
-        client: "Kesko",
-        dateAdded: "7.11.2024",
-        finishEstimate: "1.5.2025",
-        phase: "Planning",
+        "customer": "Kesko",
+        "dateAdded": "2023-10-15",
+        "finishEstimate": "2024-05-10",
+        "id": "p98765",
+        "manager": {
+            "firstName": "Alice",
+            "id": "u11223",
+            "isActive": true,
+            "lastName": "Johnson"
+        },
+        "members": [
+            {
+                "firstName": "John",
+                "id": "u12345",
+                "isActive": true,
+                "lastName": "Doe"
+            },
+            {
+                "firstName": "Jane",
+                "id": "u67890",
+                "isActive": false,
+                "lastName": "Smith"
+            }
+        ],
+        "phase": "Planning",
+        "title": "Test test"
     },
     {
-        title: "Membership app",
-        client: "Tommin sali",
-        dateAdded: "30.10.2024",
-        finishEstimate: "1.6.2025",
-        phase: "In progress",
-    },
-];
+        "customer": "S-ryhmä",
+        "dateAdded": "2024-03-25",
+        "finishEstimate": "2024-12-10",
+        "id": "uis928",
+        "manager": {
+            "firstName": "Tony",
+            "id": "so928e",
+            "isActive": true,
+            "lastName": "Gravy"
+        },
+        "members": [
+            {
+                "firstName": "John",
+                "id": "u12345",
+                "isActive": true,
+                "lastName": "Doe"
+            },
+            {
+                "firstName": "Jane",
+                "id": "u67890",
+                "isActive": false,
+                "lastName": "Smith"
+            }
+        ],
+        "phase": "Completed",
+        "title": "Another test"
+    }
+]
 
 const ProjectList: React.FC = () => {
+    /*const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/projects");
+            const data = await response.json();
+            setProjects(data);
+            console.log(data);
+        } catch (error) {
+            console.error("Error fetching projects:", error);
+        }
+        };
+
+        fetchProjects();
+    }, []);*/
+
     return (
         <div className={styles.componentcontainer}>
             <div className={styles.projectlist}>
                 <div className={styles.listcontainer}>
-                    {projects.map((project, index) => (
-                        <Project key={index} title={project.title} />
+                    {projects.map((project) => (
+                        <Project 
+                            key={project.id} 
+                            title={project.title} 
+                            customer={project.customer}
+                            dateAdded={project.dateAdded}
+                            finishEstimate={project.finishEstimate}
+                            phase={project.phase}
+                        />
                     ))}
                 </div>
             </div>
