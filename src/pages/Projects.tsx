1359c03ed49552d67249350be1eeb084a10f2704
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-02-09 10:03:09 +0200

    En kerro mitä tein :)

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Navbar from '../components/Navbar/Navbar.tsx';
 import Sidenav from '../components/Sidenav/Sidenav.tsx';
 import ProjectList from '../components/ProjectList/ProjectList.tsx';
-import AdminToggle from '../components/AdminToggle/AdminToggle.tsx';
 
 const Projects: React.FC = () => {
   return (
     <div>
       <Navbar />
       <Sidenav />
-      <AdminToggle />
       <main>
         <h1>Projects page</h1>
         <ProjectList />
