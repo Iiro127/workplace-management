574d5afb3d777a791102e2918dc5fb1ca855b365
Iiro Anttila <iiro.a.anttila@gmail.com>
2025-04-16 15:52:27 +0300

    dropdown

--- src/components/Projects/Project/StatusContainer/StatusContainer.tsx
+++ src/components/Projects/Project/StatusContainer/StatusContainer.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './StatusContainer.module.css'
+import { useAtom } from 'jotai';
+import { authAtom } from '../../../../atoms/authAtom.tsx'
 
 interface statusProps {
     status: string;
 }
 
 const StatusContainer: React.FC<statusProps> = ({ status }) => {
+    const [auth] = useAtom(authAtom)
+    const [currentStatus, setCurrentStatus] = useState(status);
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setCurrentStatus(e.target.value);
+    };
+
     return (
         <div className={styles.statusContainer}>
             <div className={`${styles.status} ${styles[status.toLowerCase().replace(/\s/g, '')]}`}>
                 <p>{status}</p>
             </div>
+
+            <select onChange={handleChange} value={currentStatus}>
+                <option value="Planning">Planning</option>
+                <option value="In Progress">In Progress</option>
+                <option value="Completed">Completed</option>
+            </select>
         </div>
     )
 }
